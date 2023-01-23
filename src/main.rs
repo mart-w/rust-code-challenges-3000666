2f4641bd4985b4488adcc22fa5524abd5c8d26db
Martin Wurm <martin.wurm@mart-w.de>
2023-01-23 02:57:57 +0100

    01_02 solution

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-fn unique(a: Vec<i32>) -> Vec<i32> {
-    todo!();
-}
+// fn unique(a: Vec<i32>) -> Vec<i32> {
+//     todo!();
+// }
 
 // advanced 1: use generic types
 // fn unique(a: Vec<T>) -> Vec<T> {
@@ -12,10 +12,19 @@ fn unique(a: Vec<i32>) -> Vec<i32> {
 //     todo!();
 // }
 
-// advanced 3: use iterators
-// fn unique(a: Iterable<T>) -> Vec<T> {
-//     todo!();
-// }
+fn unique<T: Ord>(a: Vec<T>) -> Vec<T> {
+    let mut out: Vec<T> = Vec::new();
+
+    for item in a {
+        if !out.contains(&item) {
+            out.push(item);
+        }
+    }
+
+    out.sort_by(|a, b| a.partial_cmp(b).unwrap());
+
+    out
+}
 
 fn main() {
     let input = vec![2, 1, 1];
@@ -23,10 +32,9 @@ fn main() {
     println!("unique items -> {:?}", answer)
 }
 
-
 #[test]
 fn empty_list() {
-    let input = vec![];
+    let input: Vec<i32> = vec![];
     let expected_output = vec![];
     let actual_output = unique(input);
     assert_eq!(actual_output, expected_output);
@@ -48,7 +56,6 @@ fn unsorted_list() {
     assert_eq!(actual_output, expected_output);
 }
 
-
 #[test]
 fn unsorted_list_with_duplicates() {
     let input = vec![1, 5, 2, 2, 1];
@@ -60,7 +67,7 @@ fn unsorted_list_with_duplicates() {
 #[test]
 fn sorted_list_with_duplicates() {
     let mut input = vec![1, 5, 2, 2, 1];
-    input.sort_by(|x,y| x.partial_cmp(y).unwrap());
+    input.sort_by(|x, y| x.partial_cmp(y).unwrap());
     let expected_output = vec![1, 2, 5];
     let actual_output = unique(input);
     assert_eq!(actual_output, expected_output);
